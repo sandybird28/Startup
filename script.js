12b89c43cb0224346f417b46256b6a06dda54825
sandybird28 <aler2893@gmail.com>
2019-08-02 23:25:50 +0300

    done

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 'use strict';
 
+$('.about-us-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 2,    
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    responsive: [
+        {
+        breakpoint: 1000,
+        settings: {
+            arrows: false,
+            slidesToShow: 3,
+            slidesToScroll: 1
+        }},
+        {
+        breakpoint: 750,
+        settings: {
+            arrows: false,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }}
+        
+    ]
+
+  });
 
 $('.clients-logos-bar').slick({
     autoplay: true,
     autoplaySpeed: 1500,
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 4,
+    slidesToShow: 5,
     arrows: false,
     responsive: [
         {
-        breakpoint: 768,
+        breakpoint: 1100,
         settings: {
             arrows: false,
-            centerMode: true,
-            centerPadding: '40px',
             slidesToShow: 3
         }
         },
@@ -22,9 +44,7 @@ $('.clients-logos-bar').slick({
         breakpoint: 480,
         settings: {
             arrows: false,
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 1
+            slidesToShow: 2
         }
         }
     ]
