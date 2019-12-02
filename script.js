dec7841af2bb41bc81b9a031ce79d86659f3247b
sandybird28 <aler2893@gmail.com>
2019-12-02 21:08:24 +0200

    deleted sliders, add scroll-to-top button

--- script.js
+++ script.js
@@ -1,96 +1,30 @@
 'use strict';
+    const navArr = document.querySelectorAll('.menu__links-item');
+    const secArr = document.querySelectorAll('section');
+    const toTop = document.querySelector('.to-top');
 
+    toTop.addEventListener('click',()=>{
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    })
 
-function slowScroll(id) { 
-    let offset = 0;
-    $('html, body').animate({ 
-         scrollTop: $(id).offset().top - offset 
-    }, 800);
-    return false; 
-} 
-
-
-$('.about-us-slider').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 2,    
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: false,
-    responsive: [
-        {
-        breakpoint: 1000,
-        settings: {
-            arrows: false,
-            slidesToShow: 3,
-            slidesToScroll: 1
-        }},
-        {
-        breakpoint: 750,
-        settings: {
-            arrows: false,
-            slidesToShow: 1,
-            slidesToScroll: 1
-        }}
-        
-    ]
-
-  });
-
-$('.clients-logos-bar').slick({
-    autoplay: true,
-    autoplaySpeed: 1500,
-    slidesToShow: 5,
-    arrows: false,
-    responsive: [
-        {
-        breakpoint: 1100,
-        settings: {
-            arrows: false,
-            slidesToShow: 3
+    window.addEventListener('scroll',() => {
+        if(window.scrollY > 300){
+            toTop.style.display = "block";
+        } else {
+            toTop.style.display = "none";
         }
-        },
-        {
-        breakpoint: 480,
-        settings: {
-            arrows: false,
-            slidesToShow: 2
+    })
+
+    navArr.forEach((el,i) =>{
+        if(i > 0 && i < 4){
+            el.addEventListener('click', ()=>{
+                secArr[i-1].scrollIntoView({block: "start", behavior: "smooth"})
+            })
+        } else {
+            el.addEventListener('click', ()=>{
+                secArr[i].scrollIntoView({block: "start", behavior: "smooth"})
+            })
+    
         }
-        }
-    ]
-    });
-
-
-$('.clients-description').slick({
-    dots: true,
-    arrows: false,
-});    
-
-$('a.start').hover(function(){
-    $(this).removeClass('fadeInUpBig');
-    $(this).addClass('tada');
-    });
-   
-$('a.start').mouseleave(function(){
-    $(this).removeClass('tada');
-    });
-
-$('.menu__icon').on('click', function() {
-    $(this).closest('.menu').toggleClass('menu_state_open');
-    });
-
-
-let windowHeight = $(window).height();
- 
-$(document).on('scroll', function() {
-    $('.service').each(function() {
-        let self = $(this),
-        height = self.offset().top + self.height();
-        if ($(document).scrollTop() + windowHeight >= height) {
-            $('.service').css('visibility', 'visible');
-            $('a .left').addClass('animated fadeInRight');
-            $('a .center').addClass('animated fadeInUp');
-            $('a .right').addClass('animated fadeInLeft')
-        }
-    });
-});
+    })
